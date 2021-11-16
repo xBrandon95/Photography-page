1648d51cdb7a74d9d969a6ba04af55ba68ed322e
Brandon Ticona <jbticona4@gmail.com>
2021-11-15 20:59:47 -0400

    landing page finished

---
+++ js/main.js
@@ -0,0 +1,77 @@
+let index = 0;
+const totalWorkItems = $('.work-item').length;
+
+$(window).on('load', function () {
+  $('.preloader').addClass('loaded');
+});
+
+$(document).ready(function () {
+  // Nav toggle
+  $('.nav-toggle').click(function () {
+    $('.header .nav').slideToggle();
+  });
+
+  $('.header .nav a').click(function () {
+    if ($(window).width() < 768) {
+      $('.header .nav').slideToggle();
+    }
+  });
+
+  // fixed header
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('.header').addClass('fixed');
+    } else {
+      $('.header').removeClass('fixed');
+    }
+  });
+
+  // set lightbox img max height
+  const wHeight = $(window).height();
+  $('.lightbox-img').css('max-height', wHeight + 'px');
+
+  // lightbox
+  $('.work-item-inner').click(function () {
+    index = $(this).parent('.work-item').index();
+    $('.lightbox').addClass('open');
+    lightboxSlideShow();
+  });
+
+  $('.lightbox .prev').click(function () {
+    if (index == 0) {
+      index = totalWorkItems - 1;
+    } else {
+      index--;
+    }
+    lightboxSlideShow();
+  });
+
+  $('.lightbox .next').click(function () {
+    if (index == totalWorkItems - 1) {
+      index = 0;
+    } else {
+      index++;
+    }
+    lightboxSlideShow();
+  });
+
+  // close lightbox
+  $('.lightbox-close').click(function () {
+    $('.lightbox').removeClass('open');
+  });
+
+  // close lightbox when clicked outside of img-box
+  $('.lightbox').click(function (event) {
+    if ($(event.target).hasClass('lightbox')) {
+      $(this).removeClass('open');
+    }
+  });
+});
+
+function lightboxSlideShow() {
+  const imgSrc = $('.work-item').eq(index).find('img').attr('data-large');
+  const category = $('.work-item').eq(index).find('h4').html();
+  $('.lightbox-img').attr('src', imgSrc);
+  $('.lightbox-category').html(category);
+  $('.lightbox-counter').html(index + 1 + '/' + totalWorkItems);
+}
